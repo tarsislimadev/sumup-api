ff3ce1deecd1f00ee13b44d83c29a1a5d4febf90
Tarsis <br.tmvdl@gmail.com>
2023-10-30 09:52:22 -0300

    Updating CORE script files

--- post.sh
+++ post.sh
@@ -6,4 +6,6 @@ PATH="${1}"
 
 QUERY="${2}"
 
-curl -X POST -sL "https://api.sumup.com/${API_VERSION}/${PATH}?${QUERY}"
+DATA="${3}"
+
+curl -X POST -sL "https://api.sumup.com/${API_VERSION}/${PATH}?${QUERY}" -H "Authorization: Bearer ${API_KEY}" -d "${DATA}"
